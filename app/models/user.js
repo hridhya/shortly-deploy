6575cea16af14a2aafdeea0fcc2a56e2ab8f69ae
Hack Reactor Students <accounts@hackreactor.com>
2015-12-30 18:16:05 -0800

    started refactoring with mongoose

--- app/models/user.js
+++ app/models/user.js
@@ -1,18 +1,28 @@
 var db = require('../config');
+var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
+var Schema = mongoose.Schema;
+
+
+var users = new Schema({
+  username: String,
+  password: String,
+  created_at: Date,
+  updated_at: Date
+
+}),
+
+var User = mongoose.model('User', users);
+
+User.comparePassword = function(attemptedPassword, callback) {
     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
       callback(isMatch);
     });
   },
+  
+
   hashPassword: function(){
     var cipher = Promise.promisify(bcrypt.hash);
     return cipher(this.get('password'), null, null).bind(this)
